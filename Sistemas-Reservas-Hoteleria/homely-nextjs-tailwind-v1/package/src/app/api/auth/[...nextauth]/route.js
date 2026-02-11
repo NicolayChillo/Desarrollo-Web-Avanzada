e2b51c00b010d937acc265a2ab959d9ea182772d
NicolayChillo <nicolaychillo13@gmail.com>
2026-02-11 09:52:01 -0500

    Update port for production and local

--- Sistemas-Reservas-Hoteleria/homely-nextjs-tailwind-v1/package/src/app/api/auth/[...nextauth]/route.js
+++ Sistemas-Reservas-Hoteleria/homely-nextjs-tailwind-v1/package/src/app/api/auth/[...nextauth]/route.js
@@ -1,19 +1,9 @@
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
-import GoogleProvider from 'next-auth/providers/google';
-import GitHubProvider from 'next-auth/providers/github';
 
 const handler = NextAuth({
   site: process.env.NEXTAUTH_URL || 'http://localhost:3000',
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
     CredentialsProvider({
       name: 'credentials',
       credentials: {
--- Sistemas-Reservas-Hoteleria/homely-nextjs-tailwind-v1/package/src/services/api.ts
+++ Sistemas-Reservas-Hoteleria/homely-nextjs-tailwind-v1/package/src/services/api.ts
@@ -1,4 +1,5 @@
-const API_BASE_URL = 'http://localhost:9090';
+const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080';
 
 let authToken: string | null = null;
 
